8b909c4f11407c6288b2fe7544c3575381c25b0a
João Trigo Soares <joao@jtsoar.es>
2018-04-15 20:21:49 +0100

    stt interface for future use

---
+++ src/SafeToNet.Prototype.Core/Interfaces/ISttClient.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace SafeToNet.Prototype.Core.Interfaces
+{
+    /// <summary>
+    /// Interface for Speech-To-Text clients
+    /// </summary>
+    public interface ISttClient
+    {
+        /// <summary>
+        /// Parses the speech.
+        /// </summary>
+        /// <param name="speech">The speech.</param>
+        /// <returns>Task&lt;System.String&gt;.</returns>
+        Task<string> ParseSpeech(byte[] speech);
+    }
+}
